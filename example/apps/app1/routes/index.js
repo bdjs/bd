703409db90094787727d95bc94c989faa845d41c
mdemo <mds@xue.bi>
2015-06-03 16:13:35 +0800

    add logger lib

--- example/apps/app1/routes/index.js
+++ example/apps/app1/routes/index.js
@@ -5,9 +5,12 @@
 
 'use strict';
 
+var bd = require('../../../../lib/application');
+var logger = bd.logger('app1');
 
 module.exports = function (router) {
   router.get('/name', function *(next) {
+    logger.access(this.request);
     this.body = 'app';
     yield  next;
   });
